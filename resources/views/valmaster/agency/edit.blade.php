06740fbe16209398eef0781f3783d72bf0115f34
Chipango <35219186+KahiluChipango@users.noreply.github.com>
2022-09-24 16:45:52 +0200

    added upload button on edit record in agency

--- resources/views/valmaster/agency/edit.blade.php
+++ resources/views/valmaster/agency/edit.blade.php
@@ -771,7 +771,17 @@
                                         </div>
                                     </div>
 
-                                <!-- Add Button-->
+
+                                    <!-- Add Button-->
+                                    <div class="flex items-center justify-start mt-4">
+
+                                        <x-button class="cursor-pointer ml-4">
+                                            {{ __('Change Photo') }}
+                                        </x-button>
+                                    </div>
+
+
+                                    <!-- Add Button-->
                                 <div class="flex items-center justify-end mt-4 ">
 
                                     <x-button class="cursor-pointer ml-4">
